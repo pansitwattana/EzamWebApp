3d45c03da86b6695346ff1fc27a182d1e7f08de3
pansitwattana <pansit.wt@gmail.com>
2017-08-26 13:06:44 +0700

    add app

---
+++ src/routes/app/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+import App from './App';
+
+const title = 'App';
+A;
+function action() {
+  return {
+    chunks: ['app'],
+    title,
+    component: <App title={title} />,
+  };
+}
+
+export default action;
